7d969a288b42f56bb8ec62e3067d056b37a2f6a1
Junaid <junaidkabani3@gmail.com>
2023-03-31 12:07:19 -0700

    Fix link on homepage

--- pages/index.js
+++ pages/index.js
@@ -42,7 +42,7 @@ const Home = () => {
           </a>
 
           <a
-            href="https://nextjs.org/learn"
+            href="/api/graphql"
             className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
           >
             <h3 className="text-2xl font-bold">GraphQL API</h3>
