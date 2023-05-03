2333a73b946636234d3aae9ae85a0888aa7702d0
Junaid <junaidkabani3@gmail.com>
2023-05-02 21:16:32 -0700

    Revert index page

---
+++ pages/home.js
@@ -0,0 +1,23 @@
+import Head from "@modules/common/components/head"
+import FeaturedProducts from "@modules/home/components/featured-products"
+import Hero from "@modules/home/components/hero"
+import Layout from "@modules/layout/templates"
+
+const Home = () => {
+  return (
+    <>
+      <Head
+        title="Home"
+        description="Shop all available models only at the ACME. Worldwide Shipping. Secure Payment."
+      />
+      <Hero />
+      <FeaturedProducts />
+    </>
+  )
+}
+
+Home.getLayout = page => {
+  return <Layout>{page}</Layout>
+}
+
+export default Home
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,59 @@
-import Head from "@modules/common/components/head"
-import FeaturedProducts from "@modules/home/components/featured-products"
-import Hero from "@modules/home/components/hero"
-import Layout from "@modules/layout/templates"
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
 
 const Home = () => {
   return (
-    <>
-      <Head
-        title="Home"
-        description="Shop all available models only at the ACME. Worldwide Shipping. Secure Payment."
-      />
-      <Hero />
-      <FeaturedProducts />
-    </>
-  )
-}
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-Home.getLayout = page => {
-  return <Layout>{page}</Layout>
-}
+      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
+        <h1 className="text-6xl font-bold">
+          Welcome to{" "}
+          <Link legacyBehavior href="/">
+            <a className="text-blue-600">Openfront!</a>
+          </Link>
+        </h1>
 
-export default Home
+        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
+          <Link legacyBehavior href="/">
+            <a className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600">
+              <div className="flex justify-between items-start">
+                <h3 className="text-2xl font-bold">Storefront</h3>
+                <span className="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 mt-1.5 rounded ">
+                  SOON
+                </span>
+              </div>
+              <p className="mt-4 text-xl">
+                Openfront comes with a storefront that will appear here{" "}
+              </p>
+            </a>
+          </Link>
+
+          <Link legacyBehavior href="/admin">
+            <a className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600">
+              <h3 className="text-2xl font-bold">Admin UI</h3>
+              <p className="mt-4 text-xl">
+                Openfront comes with a full Admin UI
+              </p>
+            </a>
+          </Link>
+
+          <Link legacyBehavior href="/api/graphql">
+            <a className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600">
+              <h3 className="text-2xl font-bold">GraphQL API</h3>
+              <p className="mt-4 text-xl">
+                Openfront comes with a GraphQL API and Playground
+              </p>
+            </a>
+          </Link>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
