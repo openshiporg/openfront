60471dec0f2ec484ffca8119bdd97f0495384a47
Junaid <junaidkabani3@gmail.com>
2023-06-20 06:15:03 -0500

    Simplify middleware.js

--- middleware.js
+++ middleware.js
@@ -3,30 +3,29 @@ import { NextResponse } from "next/server";
 
 export async function middleware(req) {
   const { authenticatedItem: isAuth, redirectToInit } = await checkAuth(req);
-  const isInitPage = req.nextUrl.pathname.startsWith("/dashboard/init");
-  const isSignInPage = req.nextUrl.pathname.startsWith("/dashboard/signin");
+  const isInitPage = req.nextUrl.pathname.startsWith('/dashboard/init');
+  const isSignInPage = req.nextUrl.pathname.startsWith('/dashboard/signin');
 
   if (redirectToInit && !isInitPage) {
-    console.log("type 1");
-    return NextResponse.redirect(new URL("/dashboard/init", req.url));
-  } else if (!redirectToInit && isInitPage) {
-    // Redirect away from /dashboard/init if redirectToInit is not true
-    console.log("type new");
-    return NextResponse.redirect(new URL("/dashboard", req.url));
-  } else if (isSignInPage) {
-    console.log("type 2");
+    console.log("1")
+    return NextResponse.redirect(new URL('/dashboard/init', req.url));
+  }
 
+  if (!redirectToInit && isInitPage) {
+    console.log("2")
+    return NextResponse.redirect(new URL('/dashboard', req.url));
+  }
+
+  if (isSignInPage) {
+    console.log("3")
     if (isAuth) {
-      console.log("type 3");
-
-      return NextResponse.redirect(new URL("/dashboard", req.url));
+      return NextResponse.redirect(new URL('/dashboard', req.url));
     }
-
     return null;
-  } else if (!isAuth && !isInitPage) {
-    // Added a condition to check if the user is not on the '/dashboard/init' page
-    console.log("type 4");
+  }
 
+  if (!isAuth && !isInitPage) {
+    console.log("4")
     let from = req.nextUrl.pathname;
     if (req.nextUrl.search) {
       from += req.nextUrl.search;
