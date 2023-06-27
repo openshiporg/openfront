199826af3ed6601c03e2e6041c32b617c5ebedcc
Junaid <junaidkabani3@gmail.com>
2023-06-26 19:56:58 -0500

    Fix default value

--- keystone/themes/KeystoneUI/components/AdminLink/index.js
+++ keystone/themes/KeystoneUI/components/AdminLink/index.js
@@ -5,7 +5,7 @@ import Link from "next/link";
 import { jsx } from "@keystone-ui/core";
 
 export const AdminLink = ({ href, children, ...props }) => {
-  const adminPath = process.env.NEXT_PUBLIC_ADMIN_PATH || "/admin";
+  const adminPath = process.env.NEXT_PUBLIC_ADMIN_PATH || "/dashboard";
   const adminHref = `${adminPath}${href}`;
 
   return (
