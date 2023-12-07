969bc0af05ec1a656ab393b69be8918f490e9544
Junaid <junaidkabani3@gmail.com>
2023-12-07 02:57:54 -0600

    Update FilterAdd code

--- keystone/themes/Tailwind/components/FilterAdd/index.js
+++ keystone/themes/Tailwind/components/FilterAdd/index.js
@@ -31,19 +31,23 @@ export function FilterAdd({ listKey, filterableFields }) {
 
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline" className="border-dashed flex items-center">
+      <DropdownMenuTrigger>
+        <Button
+          variant="outline"
+          className="border-dashed flex items-center"
+          onClick={() => setIsOpen(true)}
+        >
           <ListFilterIcon className="mr-2 w-4 h-4" />
           Filter
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent align="end" className="w-[200px]">
+      {isOpen && (
         <FilterAddPopoverContent
           onClose={() => setIsOpen(false)}
           listKey={listKey}
           filterableFields={filterableFields}
         />
-      </DropdownMenuContent>
+      )}
     </DropdownMenu>
   );
 }
@@ -102,120 +106,122 @@ function FilterAddPopoverContent({ onClose, listKey, filterableFields }) {
   };
 
   return (
-    <form
-      onSubmit={(event) => {
-        event.preventDefault();
-        if (state.kind === "filter-value") {
-          router.push(
-            pathname +
-              "?" +
-              new URLSearchParams({
-                ...query,
-                [`!${state.fieldPath}_${state.filterType}`]: JSON.stringify(
-                  state.filterValue
-                ),
-              })
-          );
-          onClose();
-        }
-      }}
-    >
-      <div className="flex justify-between">
-        {state.kind !== "selecting-field" && (
-          <Button
-            onClick={() => {
-              setState({ kind: "selecting-field" });
-            }}
-            size="xs"
-            variant="outline"
-          >
-            <div className="sr-only">Back</div>
-            <ChevronLeftIcon className="w-4 h-4" />
-          </Button>
-        )}
-        <DropdownMenuLabel className="pb-0">
-          {(() => {
-            switch (state.kind) {
-              case "selecting-field": {
-                return "Filter";
-              }
-              case "filter-value": {
-                return list.fields[state.fieldPath].label;
-              }
-            }
-          })()}
-        </DropdownMenuLabel>
-      </div>
-      <DropdownMenuSeparator />
-      <ScrollArea vpClassName="max-h-72">
-        {state.kind === "selecting-field" &&
-          Object.keys(filtersByFieldThenType).map((fieldPath) => (
-            <button
-              key={fieldPath}
-              className="rounded-sm px-2 py-1.5 text-sm w-full text-left hover:bg-gray-100 dark:hover:bg-gray-900"
-              onClick={() => handleSelectField(fieldPath)}
-            >
-              {list.fields[fieldPath].label}
-            </button>
-          ))}
-        {state.kind === "filter-value" && (
-          <Select
-            onValueChange={(filterType) => {
-              setState({
-                kind: "filter-value",
-                fieldPath: state.fieldPath,
-                filterValue:
-                  fieldsWithFilters[state.fieldPath].controller.filter.types[
-                    filterType
-                  ].initialValue,
-                filterType: filterType,
-              });
-            }}
-          >
-            <SelectTrigger className="mb-2">
-              <SelectValue>
-                {filtersByFieldThenType[state.fieldPath][state.filterType]}
-              </SelectValue>
-            </SelectTrigger>
-            <SelectContent>
-              {Object.keys(filtersByFieldThenType[state.fieldPath]).map(
-                (filterType) => (
-                  <SelectItem key={filterType} value={filterType}>
-                    {filtersByFieldThenType[state.fieldPath][filterType]}
-                  </SelectItem>
-                )
-              )}
-            </SelectContent>
-          </Select>
-        )}
-        {state.kind == "filter-value" &&
-          (() => {
-            const { Filter } =
-              fieldsWithFilters[state.fieldPath].controller.filter;
-            return (
-              <Filter
-                type={state.filterType}
-                value={state.filterValue}
-                onChange={(value) => {
-                  setState((state) => ({
-                    ...state,
-                    filterValue: value,
-                  }));
-                }}
-              />
+    <DropdownMenuContent align="end" className="w-[200px]">
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          if (state.kind === "filter-value") {
+            router.push(
+              pathname +
+                "?" +
+                new URLSearchParams({
+                  ...query,
+                  [`!${state.fieldPath}_${state.filterType}`]: JSON.stringify(
+                    state.filterValue
+                  ),
+                })
             );
-          })()}
-      </ScrollArea>
-      {state.kind == "filter-value" && (
-        <div className="flex justify-between mt-4 pt-2 pb-1 px-1 border-t">
-          <Button onClick={onClose} variant="ghost" size="xs">
-            Cancel
-          </Button>
-          <Button type="submit" size="xs">
-            Apply
-          </Button>
+            onClose();
+          }
+        }}
+      >
+        <div className="flex justify-between">
+          {state.kind !== "selecting-field" && (
+            <Button
+              onClick={() => {
+                setState({ kind: "selecting-field" });
+              }}
+              size="xs"
+              variant="outline"
+            >
+              <div className="sr-only">Back</div>
+              <ChevronLeftIcon className="w-4 h-4" />
+            </Button>
+          )}
+          <DropdownMenuLabel className="pb-0">
+            {(() => {
+              switch (state.kind) {
+                case "selecting-field": {
+                  return "Filter";
+                }
+                case "filter-value": {
+                  return list.fields[state.fieldPath].label;
+                }
+              }
+            })()}
+          </DropdownMenuLabel>
         </div>
-      )}
-    </form>
+        <DropdownMenuSeparator />
+        <ScrollArea vpClassName="max-h-72">
+          {state.kind === "selecting-field" &&
+            Object.keys(filtersByFieldThenType).map((fieldPath) => (
+              <button
+                key={fieldPath}
+                className="rounded-sm px-2 py-1.5 text-sm w-full text-left hover:bg-gray-100 dark:hover:bg-gray-900"
+                onClick={() => handleSelectField(fieldPath)}
+              >
+                {list.fields[fieldPath].label}
+              </button>
+            ))}
+          {state.kind === "filter-value" && (
+            <Select
+              onValueChange={(filterType) => {
+                setState({
+                  kind: "filter-value",
+                  fieldPath: state.fieldPath,
+                  filterValue:
+                    fieldsWithFilters[state.fieldPath].controller.filter.types[
+                      filterType
+                    ].initialValue,
+                  filterType: filterType,
+                });
+              }}
+            >
+              <SelectTrigger className="mb-2">
+                <SelectValue>
+                  {filtersByFieldThenType[state.fieldPath][state.filterType]}
+                </SelectValue>
+              </SelectTrigger>
+              <SelectContent>
+                {Object.keys(filtersByFieldThenType[state.fieldPath]).map(
+                  (filterType) => (
+                    <SelectItem key={filterType} value={filterType}>
+                      {filtersByFieldThenType[state.fieldPath][filterType]}
+                    </SelectItem>
+                  )
+                )}
+              </SelectContent>
+            </Select>
+          )}
+          {state.kind == "filter-value" &&
+            (() => {
+              const { Filter } =
+                fieldsWithFilters[state.fieldPath].controller.filter;
+              return (
+                <Filter
+                  type={state.filterType}
+                  value={state.filterValue}
+                  onChange={(value) => {
+                    setState((state) => ({
+                      ...state,
+                      filterValue: value,
+                    }));
+                  }}
+                />
+              );
+            })()}
+        </ScrollArea>
+        {state.kind == "filter-value" && (
+          <div className="flex justify-between mt-4 pt-2 pb-1 px-1 border-t">
+            <Button onClick={onClose} variant="ghost" size="xs">
+              Cancel
+            </Button>
+            <Button type="submit" size="xs">
+              Apply
+            </Button>
+          </div>
+        )}
+      </form>
+    </DropdownMenuContent>
   );
 }
