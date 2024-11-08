b7fc71a2f0e45b34926f0c1bb4ba1816947c9141
Junaid <junaidkabani3@gmail.com>
2024-11-08 03:58:35 -0600

    Update next config

--- next.config.js
+++ next.config.js
@@ -31,7 +31,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: process.env.S3_ENDPOINT ? process.env.S3_ENDPOINT.replace(/^https?:\/\//, '').replace(/:\d+$/, '') : '',
+        hostname: process.env.S3_ENDPOINT ? process.env.S3_ENDPOINT.replace(/^https?:\/\//, '').replace(/:\d+$/, '') : '/',
         port: '',
         pathname: '/**',
       },
