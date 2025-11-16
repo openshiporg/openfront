469ba4bc091ac5191074e837d3058b145056868e
Junaid <junaidkabani3@gmail.com>
2025-11-16 12:00:04 -0600

    perf(storefront): remove quality prop from product thumbnail images

--- features/storefront/modules/products/components/thumbnail/index.tsx
+++ features/storefront/modules/products/components/thumbnail/index.tsx
@@ -60,7 +60,6 @@ const ImageOrPlaceholder = ({
       alt="Thumbnail"
       className="absolute inset-0 object-cover object-center"
       draggable={false}
-      quality={50}
       sizes="(max-width: 576px) 280px, (max-width: 768px) 360px, (max-width: 992px) 480px, 800px"
       fill
       suppressHydrationWarning
