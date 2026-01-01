28599fffbca710596fdf985f1975feaae3af785e
Junaid <junaidkabani3@gmail.com>
2026-01-01 12:16:52 +0300

    feat: add loading components for cart, category, and order confirmation pages

---
+++ app/(storefront)/[countryCode]/(main)/cart/loading.tsx
@@ -0,0 +1,5 @@
+import SkeletonCartPage from "@/features/storefront/modules/skeletons/templates/skeleton-cart-page"
+
+export default function CartLoading() {
+  return <SkeletonCartPage />
+}
---
+++ app/(storefront)/[countryCode]/(main)/order/confirmed/[id]/loading.tsx
@@ -0,0 +1,5 @@
+import SkeletonOrderConfirmed from "@/features/storefront/modules/skeletons/templates/skeleton-order-confirmed"
+
+export default function OrderConfirmedLoading() {
+  return <SkeletonOrderConfirmed />
+}
